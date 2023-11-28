c55390d2247d00143be05b276c381b3b976a8b7f
Fei Yu Guan <victorkwan1206@yahoo.com>
2023-11-27 22:29:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,11 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jun 29 19:04:16 2022
-
-@author: guanfei1
-"""
-
 import gym
 import numpy as np
 from gym import wrappers
 from DDPG import *
 import matplotlib.pyplot as plt
 if __name__ == '__main__':
-    env = gym.make('InvertedPendulum-v4', render_mode='human')
-    agent = DDPG(env)
+    env = gym.make('LunarLander-v2')
+    agent = DDPG(env, 192)
     ys = []
     eps = 10000
     xs = list(range(eps))
@@ -21,10 +13,12 @@ if __name__ == '__main__':
     for j in range(eps):
         done = False
         episode_reward = 0
-        s = env.reset()[0]
+        s = env.reset()
         while not done:
             action = agent.act(s)
-            s_, r, done, _, _ = env.step(action)
+            env.render()
+
+            s_, r, done, _= env.step(action)
             agent.replay_buffer.append((s, action, s_, r, done))
             s = s_
             episode_reward += r
@@ -37,6 +31,9 @@ if __name__ == '__main__':
         print("Episode Reward", episode_reward, ", Average Reward", 
               mean)
     plt.plot(xs, ys)
+    
+    
+
     
     
 
